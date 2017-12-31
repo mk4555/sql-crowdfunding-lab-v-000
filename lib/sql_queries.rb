29781f6bd601e2fe51b605a4b95553e2c210ab45
Min Kim <mk4555@nyu.edu>
2017-12-31 18:00:57 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "SELECT users.name, SUM(pledges.amount) AS total_pledge_amount
 FROM users
 LEFT JOIN pledges
-ON pledges.project_id = projects.id
+ON users.id = pledges.user_id
 GROUP BY users.name
 ORDER BY total_pledge_amount"
 end
